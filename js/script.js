4a5a5843b3036afa19a5f3dbdc066858a3286007
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-03-09 16:05:52 +0000

    Added and edited

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Stella S
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = 'Surprise! You Pressed the button now this text has appeared. So cool! This is my first time coding with JS. Hope you liked it.'
+}
